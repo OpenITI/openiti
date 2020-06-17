b2a2a02f033afc453b1e42e1c183e0d70f2a8c4d
pverkind <peter.verkinderen@gmail.com>
2020-06-17 18:12:45 +0200

    Add documentation

--- openiti/new_books/convert/html_converter_noorlib.py
+++ openiti/new_books/convert/html_converter_noorlib.py
@@ -13,11 +13,11 @@ NoorlibHtmlConverter's main methods are inherited from the GenericConverter:
 
 * convert_file(source_fp): basic procedure for converting an html file.
 * convert_files_in_folder(source_folder): convert all html files in the folder
-    (calls convert_file)
+    (calls convert_file for each html file)
 
 Overview of the methods of these classes:
 (methods of GenericConverter are inherited by GenericHtmlConverter;
-and methods of GenericHtmlConverter are inherited by EShiaHtmlConverter.
+and methods of GenericHtmlConverter are inherited by NoorlibHtmlConverter.
 Methods of the superclass with the same name
 in the subclass are overwritten by the latter)
 
@@ -30,7 +30,7 @@ convert file                       (inherited)                (inherited)
 make_dest_fp                       (inherited - generic!)     (inherited - generic!)
 get_metadata (dummy)               (inherited - dummy!)       get_metadata
 get_data                           get_data                   (inherited)
-pre_process                        (inherited)                (inherited)
+pre_process                        (inherited)                pre_process
 add_page_numbers (dummy)           (inherited - dummy!)       add_page_numbers
 add_structural_annotations (dummy) add_structural_annotations add_structural_annotations
 remove_notes (dummy)               remove_notes               remove_notes
@@ -51,6 +51,7 @@ to convert the html tags to OpenITI annotations.
 Examples:
     >>> from html_converter_noorlib import NoorlibHtmlConverter
     >>> conv = NoorlibHtmlConverter()
+    >>> conv.dest_folder = "converted"
     >>> conv.VERBOSE = False
     >>> folder = r"test/"
     >>> conv.convert_file(folder+"86596.html")
@@ -73,28 +74,47 @@ from openiti.new_books.convert.helper import html2md_noorlib
 class NoorlibHtmlConverter(GenericHtmlConverter):
 
     def pre_process(self, text):
-        def remove_html_elements(soup, tag, class_=None, contains_str=None):
-            """Remove all html elements with tag `tag` and class `class_`"""
+        """Remove superfluous elements from the html file before processing."""
+
+        def remove_html_elements(soup, tag_name, class_=None, contains_str=None):
+            """Remove all html elements with tag `tag` and class `class_` \
+            if they contain `contains_str`
+            
+            Args:
+                soup (BeautifulSoup object): BeautifulSoup representation
+                    of the html file
+                tag_name (str): name of the tag that needs to be removed
+                    (e.g., "p", "div", "span"). 
+                class_ (str): class of the tag that needs to be removed.
+                    Defaults to None. If None, all `tag_name` elements
+                    will be removed, regardless of their class.
+                contains_str (str): defaults to None. If not None,
+                    `tag_name` tags will only be removed if the text within them
+                    contains the `contains_str` string.
+            """
             if class_:
-                elements = soup.find_all(tag, class_=class_)
+                elements = soup.find_all(tag_name, class_=class_)
             else:
-                elements = soup.find_all(tag)
+                elements = soup.find_all(tag_name)
             for el in elements:
                 if contains_str:
                     if contains_str in el.text:
                         el.extract()
                 else:
                     el.extract()
+        
         text = super().pre_process(text)
+        
         # attach separated wa- and a- prefixes to the following word: 
         text = re.sub(r"\b([وأ])[\s~]+", r"\1", text)
+
+        # remove superfluous html elements: 
         soup = BeautifulSoup(text)
         remove_html_elements(soup, "style")
         remove_html_elements(soup, "title")
         remove_html_elements(soup, "div", class_="imageArea")
         remove_html_elements(soup, "span", class_="PageTitle")
         remove_html_elements(soup, "span", class_="magsImg")
-        #remove_html_elements(soup, "span", class_="OriginalDigit")
         remove_html_elements(soup, "a", class_="ayah-outer-link")
         remove_html_elements(soup, "div", class_="PageText",
                              contains_str="Page Is Empty")
@@ -104,16 +124,16 @@ class NoorlibHtmlConverter(GenericHtmlConverter):
             fn_line.insert_after("FOOTNOTES")
         text = soup.prettify()
         
-        
         return text
+    
 
     def get_metadata(self, text):
-        """Gets the metadata from the first td in the html.
+        """Gets the metadata from the first textArea div in the html.
 
-        The metadata in eShia texts are in the first div with class 'textArea'
+        The metadata in noorlib texts is in the first div with class 'textArea'
         (which represents the title page of the book).
         The only relevant metadata are title and publisher,
-        which are in a <p> element. 
+        which are in a <p> element, in which lines are separated with <br/>. 
 
         Args:
             text (str): text of the file that contains both data and metadata
@@ -125,14 +145,10 @@ class NoorlibHtmlConverter(GenericHtmlConverter):
         soup = BeautifulSoup(text)
         meta_div = soup.find("div", class_="textArea") # metadata is in first textArea div
         meta = [line.strip() for line in re.split("<br ?/?>", meta_div.prettify())]
-        #print(meta)
         title = [re.sub(".+?</b>", "", line, flags=re.DOTALL) for line in meta \
                  if "Book title" in line or "عنوان کتاب" in line or "اسم الكتاب" in line][0].strip()
         publisher = [re.sub(".+?</b>", "", line, flags=re.DOTALL) for line in meta \
                      if "Publisher" in line or "نام ناشر" in line or "اسم الناشر" in line][0].strip()
-        #print("title:", title)
-        #print("publisher:", publisher)
-        #input("Continue?")
 
         metadata =  "#META# 020.BookTITLE\t:: {}\n".format(title)
         metadata += "#META# 043.EdPUBLISHER\t:: {}\n".format(publisher)
@@ -141,14 +157,32 @@ class NoorlibHtmlConverter(GenericHtmlConverter):
         metadata = self.magic_value + metadata + self.header_splitter
         return metadata
 
+
     def add_page_numbers(self, text, source_fp):
-        """Convert the page numbers in the text into OpenITI mARkdown format"""
+        """Convert the page numbers in the text into OpenITI mARkdown format
+
+        In noorlib texts, the page numbers are in the page header
+        (<div class="PageHead">).
+        Volume numbers are not mentioned in the html files,
+        but every volume is a different html file
+        and volume numbers should be marked in the file names as VOLxxx.
+
+        The script gets the volume number from the file name
+        and the page number from the page header,
+        joins these together in the OpenITI page number format PageVxxPxxx
+        and adds this into the html at the end of the page.
+
+        It also deletes the page header after extracting the page number.
+        """
+
+        # try to get the volume number from the filename:
         try:
             vol_no = int(re.findall("VOL(\d+)", source_fp)[0])
             vol_no = "PageV{:02d}P{}".format(vol_no, "{:03d}")
         except:
             vol_no = "PageV01P{:03d}"
 
+        # add the page number
         soup = BeautifulSoup(text)
         for page in soup.find_all("div", class_="PageText"):
             try:
@@ -165,12 +199,16 @@ class NoorlibHtmlConverter(GenericHtmlConverter):
 
 
     def remove_notes(self, text):
-        """Remove footnotes from text, and format them as endnotes
+        """Remove footnotes from text, and format them as endnotes.
 
-        Footnotes are indicated in different ways in eShia texts;
-        the only communality seems to be that a <hr/> tag
-        splits off the critical apparatus from the text.
-        We will use this feature to split off the text from the footnotes.
+        Footnotes in noorlib html files are below a horizontal line
+        <hr class="footnoteLine" />, 
+        each footnote in a <div class="footnote">(footnote text)</div>
+        
+        This function extracts the footnotes from the texts
+        and turns them into endnotes.
+        The markers that indicate the location of the notes
+        within the text are not removed.
         """
         split_text = re.split("(PageV\d+P\d+)", text)
         text = []
@@ -197,25 +235,35 @@ class NoorlibHtmlConverter(GenericHtmlConverter):
         notes = self.endnote_splitter + notes
         return text, notes 
                     
-    #def convert_html2md(self, html):
+
     def add_structural_annotations(self, html):
         """Convert html to mARkdown text using a html2md converter."""
         text = html2md_noorlib.markdownify(html)
         return text             
 
+
     def post_process(self, text):
-        #empty = """ *اين صفحه در كتاب اصلي بدون متن است / هذه الصفحة فارغة في النسخة المطبوعة *"""
-        #text = re.sub(empty, "\n", text)
-        text = re.sub("(PageV\d+P\d+)\s*PageV\d+P\d+", r"\1", text)
+        """Deal with formatting probles introduced during the conversion process."""
         text = super().post_process(text)
+
+        # remove page numbers of empty pages:
+        text = re.sub("(PageV\d+P\d+)\s*PageV\d+P\d+", r"\1", text)
+
+        # remove empty paragraphs:
         text = re.sub(r"[\r\n]+# *[\r\n]+", "\n", text)
+
+        # adjust spacing after closing brackets and punctuation:
         fmt = ")»،؛:.!؟\-"
         fmt2 = fmt + "\d\s"
         text = re.sub("([{}]+)([^{}])".format(fmt, fmt2), r"\1 \2", text)
-        text = re.sub("(\w)([(«])", r"\1 \2", text)
         text = re.sub("\) ([{}])".format(fmt), r")\1", text)
-        #text = re.sub("(# [^%]+%~%.+)[\r\n]+(# [^%]+%~%.+)", r"\1\n\2", text)
+
+        # adjust spacing before opening brackets:
+        text = re.sub("(\w)([(«])", r"\1 \2", text)
+
+        # remove superfluous new lines before a new paragraph/page number
         text = re.sub("[\r\n]+(# |Page)", r"\n\1", text)
+
         return text
 
 if __name__ == "__main__":
@@ -242,10 +290,8 @@ if __name__ == "__main__":
         for fn in files:
             fp = os.path.join(root, fn)
             if fp.endswith("html"):
-                print("converting", fp)
+                #print("converting", fp)
                 if fp not in errors:
                     conv.convert_file(fp)
     for fp in errors:
         conv.convert_file(fp)
-#NB: all converted files are in G:\London\OpenITI\new\NoorLib\2020-06-16\0557IbrahimBinHusaynHamidi\converted
-    #conv.convert_files_in_folder(folder)
