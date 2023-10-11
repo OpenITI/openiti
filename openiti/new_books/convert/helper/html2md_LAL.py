3a912055ea8d0dfee6171a4d9815e6806676f6bb
pverkind <peter.verkinderen@gmail.com>
2023-10-11 11:24:14 +0200

    various improvements for formatting Library of Arabic Literature XML files

--- openiti/new_books/convert/helper/html2md_LAL.py
+++ openiti/new_books/convert/helper/html2md_LAL.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
 
 from openiti.new_books.convert.helper import html2md
 from openiti.new_books.convert.helper.html2md import *  # import all constants!
-
+from openiti.helper.quran import normalise_quran, special_letters_regex
 
 class LALHtmlConverter(html2md.MarkdownConverter):
     """Convert LAL library html to OpenITI mARkdown."""
@@ -73,31 +73,91 @@ class LALHtmlConverter(html2md.MarkdownConverter):
 ##        self.class_dict = dict()
 ##        self.class_dict["linebreak"] = '\n'
 
+    def convert(self, html):
+        """Convert XML to markdown.
+
+        NB: replaces the parent class's convert method because it uses an html parser rather than xml!
+
+        # We want to take advantage of the html5 parsing, but we don't actually
+        # want a full document. Therefore, we'll mark our fragment with an id,
+        # create the document, and extract the element with the id.
+        """
+
+        html = wrapped % html
+        #soup = BeautifulSoup(html, 'html.parser')
+        soup = BeautifulSoup(html, 'xml')
+        if 'strip' in self.options and self.options["strip"]:
+            for tag in self.options["strip"]:
+                [t.decompose() for t in soup.find_all(tag)]
+        text = self.process_tag(soup.find(id=FRAGMENT_ID), children_only=True)
+
+##        # post-processing: remove unneeded blank lines and spaces:
+##
+##        # remove leading and trailing spaces in lines:
+##        text = re.sub(r" *(\n+) *", r"\1", text)
+##        # remove unwanted additional spaces and lines:
+##        text = re.sub(r"\n{3,}", r"\n\n", text)
+##        text = re.sub(r" +", r" ", text)
+##        # remove blank lines marked with "DELETE_PREVIOUS_BLANKLINES" tag
+##        text = re.sub(r"\n+DELETE_PREVIOUS_BLANKLINES", "", text)
+##        # replace placeholders for spaces in tables:
+##        text = re.sub("ç", " ", text)
+##        return text
+        return self.post_process_md(text)
+
     def convert_div(self, el, text):
         """Convert div tags (structural markup)"""
+        # def alpha2num(s):
+        #     """Convert alphabetical part of an ID to numerical"""
+        #     # this approach doesn't work for things like 1.b.2!
+        #     alpha = re.findall("[a-zA-Z]+$", s) 
+        #     # no conversion needed if the ID does not contain letters:
+        #     if not alpha:
+        #         return s
+        #     # calculate the numerical value of an alphabetic string (a=1, aa=27, ...)
+        #     alphabet = "abcdefghijklmnopqrstuvwxyz"
+        #     num = [alphabet.index(c.lower())+1 for c in alpha[0]]
+        #     sum_num = 0
+        #     for i,n in enumerate(num[::-1]):
+        #         sum_num += (26**i)*n
+        #     return s[:-len(alpha[0])] + str(sum_num)
+
         def alpha2num(s):
-            alpha = re.findall("[a-zA-Z]+$", s)
+            """Convert alphabetical part of an ID to numerical"""
+            alpha = re.findall("[a-zA-Z]", s) 
+            # no conversion needed if the ID does not contain letters:
             if not alpha:
                 return s
+            # calculate the numerical value of an alphabetic string (a=1, aa=27, ...)
             alphabet = "abcdefghijklmnopqrstuvwxyz"
-            num = [alphabet.index(c.lower())+1 for c in alpha[0]]
-            sum_num = 0
-            for i,n in enumerate(num[::-1]):
-                sum_num += (26**i)*n
-            return s[:-len(alpha[0])] + str(sum_num)
+            num_s = []
+            for el in s.split("."):
+                if re.findall("[a-zA-Z]", el):
+                    num = [alphabet.index(c.lower())+1 for c in el]
+                    sum_num = 0
+                    for i,n in enumerate(num[::-1]):
+                        sum_num += (26**i)*n
+                    num_s.append(str(sum_num))
+                else:
+                    num_s.append(el)
+            return ".".join(num_s)
 
         if "type" in el.attrs and "prelim" in el["type"]:
             return "\n### |PARATEXT|" + text
         
-        #try:
-        #    n = el["n"]
-        #except:
         # use xml:id (e.g., xml:id="ARA.section.4.1.2") to get the section number
         xml_id = el["xml:id"]
         xml_id = re.sub("\.(\d+)([a-zA-Z]+)$", r".\1.\2", xml_id)            
         #n = ".".join(xml_id.split(".")[2:])
-        n = re.findall("(?:\d\.)*[\da-zA-Z]+$", xml_id)[0]
-        n = alpha2num(n)
+        # convert the alphabetical parts of a section ID (e.g. 4.1.b) to numbers (e.g., 4.1.2)
+        #n = re.findall("(?:\d+\.)*[\da-zA-Z]+$", xml_id)[0] # this approach does not work for things like 4.b.2...
+        try:
+            n = ".".join(xml_id.split(".")[2:])
+            n = alpha2num(n)
+        except Exception as e:
+            print(xml_id, e)
+            n = ""
+        # calculate the hierarchical level of the header (that is, the number of pipes in the ### |+ tag)
         level = len(n.split("."))
         header = ""
         for i, c in enumerate(el.findChildren()):
@@ -105,12 +165,16 @@ class LALHtmlConverter(html2md.MarkdownConverter):
                 header += " " + c.text.strip()
             elif c.name in ("div", "p") or i > 4:
                 break
-        #if not header:
-        #    return "\n[{}]{}".format(n, text)
-        #else:
+
         if "type" in el.attrs and el["type"] != "section":
+            #return "\n### {} {}{}".format(level*"|", header, text)
             #return "\n### {} {}{}{}".format(level*"|", n.split(".")[-1], header, text)
-            return "\n### {} {}{}".format(level*"|", header, text)
+            #return "\n### {} {}{}{}".format(level*"|", n, header, text)
+            if header:
+                header_regex = re.sub("\s+", r"\\s*", header)
+                text = re.sub(header_regex, "", text, count=1)
+                return "\n### {} {} {}\n{}".format(level*"|", n, header, text)
+            return "\n### {} {} {}".format(level*"|", n, text)
         else:
             return "\n[{}]{}".format(n, text)
         
@@ -145,6 +209,7 @@ class LALHtmlConverter(html2md.MarkdownConverter):
         for q in el.find_all("quote"):
             quote.append(q.text.strip())
         quote = " * ".join(quote)
+        quote = normalise_quran(quote, post_process=True)
         qid = []
         try:    
             for pointer in el.find_all("ptr"):
@@ -164,6 +229,8 @@ class LALHtmlConverter(html2md.MarkdownConverter):
 
     def convert_p(self, el, text):
         """Convert p tags (paragraphs)"""
+        if "n" in el.attrs:
+            return "\n# " + el["n"] + " " + text
         if "rend" in el.attrs:
             if "isnad" in el["rend"]:
                 #return "\n# @ISNAD@ "+tag.text.strip()
