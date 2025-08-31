733cbd43d66be6eb4ddeeb15fee2b7776b255238
Peter <peter.verkinderen@gmail.com>
2025-08-31 18:36:48 +0100

    Delete build and dist folders

--- build/lib/openiti/test/import_test.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    from os import sys, path
-    root_folder = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
-    sys.path.append(root_folder)
-
-from openiti.helper import ara
